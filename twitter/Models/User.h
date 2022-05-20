a66b451df65b089ecfcb13ddfdd0b940cc1ca65d
Marty N <mdrnodado@gmail.com>
2022-05-20 12:35:56 -0700

    created user and tweet models

---
+++ twitter/Models/User.h
@@ -0,0 +1,25 @@
+//
+//  User.h
+//  twitter
+//
+//  Created by Marty Nodado on 5/20/22.
+//  Copyright © 2022 Emerson Malca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface User : NSObject
+
+// MARK: Properties
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSString *screenName;
+@property (nonatomic, strong) NSString *profilePicture;
+
+// MARK: Initializer Method
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary;
+
+@end
+
+NS_ASSUME_NONNULL_END
