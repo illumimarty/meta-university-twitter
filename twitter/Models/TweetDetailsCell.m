2fbc4e52c56d738d99956ea55418a3d7a38bf357
Marty Nodado <mdrnodado@gmail.com>
2022-06-18 20:34:27 -0700

    added clickable links; refactored label into textview

--- twitter/Models/TweetDetailsCell.m
+++ twitter/Models/TweetDetailsCell.m
@@ -13,6 +13,7 @@
 - (void)awakeFromNib {
     [super awakeFromNib];
     // Initialization code
+    
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
@@ -30,12 +31,15 @@
     NSData *urlData = [NSData dataWithContentsOfURL:url];
 
     self.usernameLabel.text = user.name;
-    self.tweetLabel.text = tweet.text;
+    [self.tweetLabel setText:tweet.text];
+//    self.tweetLabel.text = tweet.text;
 //    self.dateLabel.text = tweet.createdAtString; // formatted date to DD/MM/YY
     self.profileImageView.image = [UIImage imageWithData:urlData];
     self.screennameLabel.text = [NSString stringWithFormat:@"@%@", user.screenName];
 //    self.favoriteCountLabel.text = [NSString stringWithFormat:@"%d", tweet.favoriteCount];
 //    self.retweetCountLabel.text = [NSString stringWithFormat:@"%d", tweet.retweetCount];
+    
+    
 }
 
 
